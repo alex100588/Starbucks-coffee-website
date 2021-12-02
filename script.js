9e020e323e9fbf2a26787aad38941a480f2ed465
alex100588 <49831245+alex100588@users.noreply.github.com>
2021-12-02 10:40:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
-function navToggle() {
+btn.addEventListener('click', ()=>{
   btn.classList.toggle('open')
   nav.classList.toggle('hidden')
   document.body.classList.toggle('no-scroll')
-}
+})
 
-btn.addEventListener('click', navToggle)
